8ee499c2e341119aeaebb1f2513d0e5ffb450ea2
PANbl4 <ponadeks@gmail.com>
2025-09-16 21:35:15 +0300

    lab01

---
+++ src/lab01/02_sum_avg.py
@@ -0,0 +1,3 @@
+number01 = float(input("a: ").replace(",", "."))
+number02 = float(input("b: ").replace(",", "."))
+print(f"sum={number01 + number02}; avg={round(((number01 + number02) / 2),2)}")
---
+++ src/lab01/05_initials_and_len.py
@@ -0,0 +1,11 @@
+FIO = input("ФИО: ")
+words = FIO.split()
+FIO_w_2spases = FIO.rstrip().lstrip()
+first_letters = []
+str_first_letters = ''
+for word in words:
+    first_letters.append(word[0])
+for letter in first_letters:
+    str_first_letters +=  letter
+print(f"Инициалы: {str_first_letters}")
+print(f"Длина (символов): {len(FIO_w_2spases)}")
