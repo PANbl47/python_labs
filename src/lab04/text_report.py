0499d46862a87811a87a9eced7b87822c7743a63
PANbl4 <ponadeks@gmail.com>
2025-10-25 17:55:46 +0300

    test

--- src/lab04/text_report.py
+++ src/lab04/text_report.py
@@ -1,15 +1,36 @@
 from src.lab04.io_txt_csv import *
 from src.lib.text import *
 
-p = read_text("C:\\VSprojects\\python_labs\\data\\lab04\\input.txt")
-p = normalize(p)
-p = tokenize(p)
-p = count_freq(p)
-p = top_n(p)
+#Задание А
+a = read_text("C:\\VSprojects\\python_labs\\data\\lab04\\input.txt")
+a = normalize(a)
+a = tokenize(a)
+a = count_freq(a)
+a = top_n(a)
 
 print(write_csv(
-    rows = p, 
+    rows = a, 
     path = "C:\\VSprojects\\python_labs\\data\\lab04\\report.csv",
     header=["Слово","Кол-во"]  
 ))
 
+#Задание B
+
+b = read_text("C:\\VSprojects\\python_labs\\data\\lab04\\input.txt")
+b = normalize(b)
+b = tokenize(b)
+b_ = b
+b = count_freq(b)
+top = top_n(b,5)
+
+print(write_csv(
+    rows = b, 
+    path = "C:\\VSprojects\\python_labs\\data\\lab04\\report.csv",
+    header=["Word","count"]  
+))
+
+print(f"Всего слов: {len(b_)}")
+print(f"Уникальных слов: {len(b)}")
+print("Топ-5:")
+for word, count in top:
+    print(f"{word}: {count}")
